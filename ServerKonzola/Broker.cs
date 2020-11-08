a876aa13c8bbb6c720a66ab1f25085421161bfbd
Your Name <you@example.com>
2020-11-08 16:08:20 +0100

    Importing projects

---
+++ ServerKonzola/Broker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Domen;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ServerKonzola
+{
+    public class Broker
+    {
+        SqlCommand komanda;
+        SqlConnection konekcija;
+        SqlTransaction transakcija;
+
+        void KonektujSe()
+        {
+            konekcija = new SqlConnection(@"");
+            komanda = konekcija.CreateCommand();
+        }
+
+        Broker()
+        {
+            KonektujSe();
+        }
+
+        static Broker instanca;
+        public static Broker dajSesiju()
+        {
+            if (instanca == null) instanca = new Broker();
+            return instanca;
+        }
+    }
+}
---
+++ ServerKonzola/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerKonzola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server s = new ServerKonzola.Server();
+            if (s.pokreniServer())
+            {
+                string x = "1. \n2. \n3.";
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
